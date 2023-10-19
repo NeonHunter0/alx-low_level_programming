e6ca3f00eb522e004a041729bc27cd4c52cec4eb
NeonHunter0 <mahfoudomary@gmail.com>
2023-10-19 23:42:19 +0100

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,55 @@
 #include "main.h"
+#include "stdio.h"
 
 /**
- * cap_string - capitalizes all words of a string.
- * @str: the string to be capitalized.
- * Return: A pointer to the changed string.
+ * isLower - determines whether ascii is lowercase
+ * @c: character
+ * Return: 1 if true, 0 if false
  */
-char *cap_string(char *str)
+
+int isLower(char c)
 {
-	int index = 0;
-	int capitalize = 1;
+	return (c >= 97 && c <= 122);
+}
+/**
+ * isDelimiter - determines whether ascii is a delimiter
+ * @c: character
+ * Return: 1 if true, 0 if false
+ */
 
-	while (str[index])
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+}
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string
+ * Return: string with capitalized words
+ */
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
 	{
-		if ((str[index] >= 'a' && str[index] <= 'z') && capitalize)
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
 		{
-			str[index] -= 32;
-			capitalize = 0;
+			*s -= 32;
+			foundDelimit = 0;
 		}
-		else if (str[index] == ' ' || str[index] == '\t' || str[index] == '\n' ||
-			str[index] == ',' || str[index] == ';' || str[index] == '.' ||
-			str[index] == '!' || str[index] == '?' || str[index] == '"' ||
-			str[index] == '(' || str[index] == ')' || str[index] == '{' ||
-			str[index] == '}')
-		{
-		capitalize = 1;
-		}
-
-		index++;
+		else
+			foundDelimit = 0;
+		s++
 	}
-	return str;
+	return (ptr);
 }
