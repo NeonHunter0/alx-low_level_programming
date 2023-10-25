78219bd4300ef4e6e144a4184e347d65da84892a
NeonHunter0 <mahfoudomary@gmail.com>
2023-10-25 23:33:05 +0100

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -53,38 +53,13 @@ int wildcmp(char *s1, char *s2)
 	if (!*s1 || !s2)
 		return (0);
 	if (*s1 == *s2)
-		ret += wildcmp(s1 + 1, s2 + 1);
-	ret += inception(s1 + 1, s2);
-	return (ret);
-}
-
-/**
- * @s1: the first string
- * @s2: the second string, can contain wildcard
- *
- * Return: 1 if identical, 0 if false
- */
-{
-	int ret = 0;
-
-	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
-		return (1);
-	if (*s1 == *s2)
-	{
-		if (!*s1)
-			return (1);
-		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
-	}
-	if (!*s1 || !s2)
-		return (0);
-	if (*s2 == '*')
 	{
 		s2 = move_past_star(s2);
 		if (!*s2)
 			return (1);
 		if (*s1 == *s2)
 			ret += wildcmp(s1 + 1, s2 + 1);
-		ret += inception(s1, s2);
+		ret += inception(s1 + 1, s2);
 		return (!!ret);
 	}
 	return (0);
