c6a45b8bc0c0a531b5bfadc45a590bc528affe49
NeonHunter0 <mahfoudomary@gmail.com>
2023-10-25 21:40:22 +0100

    c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,25 +1,21 @@
 #include "main.h"
 
 /**
- * _pow_recursion - search a string for any of a set of bytes.
+ * _pow_recursion - calculates x raised tp power of y recursively
  * @x: base
  * @y: rxposant
- * Return: pointer to the byte in `s` that
- * mathches one of the bytes in 'accept'
- * or NULL if no such byte is found.
+ * Return: x^y, -1 if y is negative
  */
 
 int _pow_recursion(int x, int y)
 {
 
-if (y < 0)
-	return (-1);
-else if (y == 1)
-	return (x);
-else if (y == 0)
-	return (1);
-
-return (x * pow_recursion(x, y - 1));
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * pow_recursion(x, y - 1));
 
 
 }
