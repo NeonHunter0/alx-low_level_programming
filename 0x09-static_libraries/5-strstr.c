6bedf6ab65801c1f6753f5bda5fda16a58d8723a
NeonHunter0 <mahfoudomary@gmail.com>
2023-10-30 21:24:42 +0100

    library

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - finds the first occurrence of a substring in a string
+ * @haystack: input string
+ * @needle: substring to find
+ * Return: pointer to the beginning of the located substring
+ * or NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+
+		haystack++;
+	}
+
+	return (NULL);
+}
