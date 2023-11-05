efc2c423b5a87baa6602523b529d56fd39d56751
NeonHunter0 <mahfoudomary@gmail.com>
2023-11-05 22:31:12 +0100

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -58,7 +58,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if ((*dog).name == NULL)
 	{
-		free(dog)
+		free(dog);
 			return (NULL);
 	}
 
