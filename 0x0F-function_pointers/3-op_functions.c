9060f325c17147794f3159ef9c104cb3818e25f7
NeonHunter0 <mahfoudomary@gmail.com>
2023-11-09 00:27:29 +0100

    all

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-clac.h"
+
+/**
+ * op_add - adds two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two integers
+ * @a: the first int
+ * @b: the second int
+ * Return: the diffrence
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multipties two integers
+ * @a: the first int
+ * @b: the second int
+ * return: the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: the first int
+ * @b: the second int
+ * Return: the quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo two integers
+ * @a: the first int
+ * @b: the second int
+ * Return: the remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
